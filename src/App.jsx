422abbabc5cbc871e0c9502fc13d4dedc9e9b735
Ashiqur2279 <ashiqurrahman2279@gmail.com>
2024-01-01 15:45:33 +0600

    add

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import Header from './components/Header/Header'
+
+function App() {
+
+  return (
+  <div className="container">
+    <Header></Header>
+  </div>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo">
+      <h2>Shop 96</h2>
+      </div>
+      <nav className="nav_container"> 
+        <ul className="nav_item">
+          <a href="#">Home</a>
+          <a href="#">Shop</a>
+          <a href="#">Categories</a>
+          <a href="#">Deals</a>
+          <a href="#">Contact</a>
+        </ul>
+        <div className="user">
+          <a href="#">Cart</a>
+          <a href="#">Sign In</a>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
